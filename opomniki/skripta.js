28323536e4663622c4954f41fca1b8e25919f94d
Miha Zadravec <mz9337@student.uni-lj.si>
2016-03-08 09:39:03 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function() {
 		var cas = document.querySelector('#cas_opomnika').value;
     	document.querySelector('#naziv_opomnika').innerHTML = '';
     	document.querySelector('#cas_opomnika').innerHTML = '';
-		document.querySelector('#opomniki').innerHTML += "<div class='opomnik'><div class='naziv_opomnika'>"+naziv+"</div><div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>";
+		document.querySelector('#opomniki').innerHTML += "<div class='opomnik senca rob'><div class='naziv_opomnika'>"+naziv+"</div><div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>";
 	}
 	
 	document.querySelector("#prijavniGumb").addEventListener("click", izvediPrijava);
@@ -25,9 +25,17 @@ window.addEventListener('load', function() {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
+			
+			console.log(cas);
 	
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
+			if(cas <= 0){
+				opomnik.innerHTML = "Opomnik!\n\nZadolžitev "+opomnik.querySelector(".naziv_opomnika").innerHTML+" je potekla!<span></span>";
+			}else if(cas > 0){
+				cas -= 1;
+				casovnik.innerHTML = cas;
+			}
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
 		}
 	}
